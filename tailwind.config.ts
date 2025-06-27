83a68fadd79f831be35666a7183b4eba14ad9a18
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-27 16:35:55 +0530

    Files generated: tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -36,6 +36,10 @@ export default {
 					DEFAULT: 'hsl(var(--destructive))',
 					foreground: 'hsl(var(--destructive-foreground))'
 				},
+        success: {
+          DEFAULT: 'hsl(var(--success))',
+          foreground: 'hsl(var(--success-foreground))'
+        },
 				muted: {
 					DEFAULT: 'hsl(var(--muted))',
 					foreground: 'hsl(var(--muted-foreground))'
@@ -68,6 +72,9 @@ export default {
 				md: 'calc(var(--radius) - 2px)',
 				sm: 'calc(var(--radius) - 4px)'
 			},
+      fontFamily: {
+        sans: ["var(--font-sans)"],
+      },
 			keyframes: {
 				'accordion-down': {
 					from: {
